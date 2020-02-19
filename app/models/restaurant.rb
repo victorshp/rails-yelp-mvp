1638078c547210eec81ae1261470bf1155505bf8
Victor Park <victor.shpark@gmail.com>
2020-02-19 15:58:00 -0300

    models and validations correct

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,6 +5,7 @@ class Restaurant < ApplicationRecord
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
-  validates :phone_number, uniqueness: true
+  validates :category, presence: true, inclusion: {
+    in: ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Restaurant.create([
+  {
+    name: 'Tuju',
+    address: 'Vila Mada',
+    category: 'french',
+    phone_number: '(11) 9999-99999'
+  },
+  {
+    name: 'Shinzushi',
+    address: 'Paraiso',
+    category: 'japanese'
+  },
+  {
+    name: 'Chifu',
+    address: 'Liberdade',
+    category: 'chinese',
+    phone_number: 'N/A'
+  },
+  {
+    name: 'China In Box',
+    address: 'everywhere',
+    category: 'chinese'
+  },
+  {
+    name: 'Lelis',
+    address: 'Jardins',
+    category: 'italian'
+  }
+])
