552a6cc98a6e64e025ae14396e54973c411d92a7
Victor Park <victor.shpark@gmail.com>
2020-02-19 15:24:39 -0300

    Models tested and correct.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  # dependent: :destroy makes it so that if your 
+  # dependent: :destroy makes it so that if you destroy that restaurant,
+  # the reviews of set restaurant are destroyed too.
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :category, presence: true, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
   validates :phone_number, uniqueness: true
 end
