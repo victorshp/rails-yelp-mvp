b854f18c0e62280aa0ed707f1cad25e6b64735df
Victor Park <victor.shpark@gmail.com>
2020-02-19 15:04:35 -0300

    models done

--- db/migrate/20200219172221_create_reviews.rb
+++ db/migrate/20200219172221_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-      t.text, :content
+      t.text :content
       t.integer :rating
       t.references :restaurant, foreign_key: true
 
